2d767f580254fd9c138b7c27003efe7f814dfdc3
bhavanareddy2014 <75924452+bhavanareddy2014@users.noreply.github.com>
2025-03-09 07:22:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+/**
+ * 
+ * <div id="parent">
+ * <div id="child">
+ * <h1>Hello Nested Element</h1>
+ * <h2>Hello H2 Tag</h2>
+ * </div>
+ * <div id=child2>
+ * <h1>Hello Nested Element</h1>
+ *  <h2>Hello H2 Tag</h2>
+ * </div>
+ * </div>
+ */
+
+const parent = React.createElement("div",{id:"parent"},
+    [
+        React.createElement("div", {id:"child"},
+            [React.createElement("h1",{},"I am H1 tag"),
+            React.createElement("h2",{},"I am H2 tag")]
+        ),
+        React.createElement("div", {id:"child2"},
+            [React.createElement("h1",{},"I am H1 tag"),
+            React.createElement("h2",{},"I am H2 tag")]
+        )
+    ]
+)
+
+const heading = React.createElement("h1", {className:"heading"}, "Hello From React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(heading);
+
+// {
+//     "type": "h1",
+//     "key": null,
+//     "ref": null,
+//     "props": {
+//         "className": "heading",
+//         "children": "Hello From React"
+//     },
+//     "_owner": null,
+//     "_store": {}
+// }
+
+root.render(parent);
+
+// object is where we give attributes {id:heading}
+// heading will return an object simple js object
